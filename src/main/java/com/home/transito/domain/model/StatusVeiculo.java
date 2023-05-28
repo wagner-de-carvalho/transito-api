675c1f5a19563ec2a4e4266fdba53ec98348081f
wagner <wagncarv@outlook.com>
2023-05-28 13:18:09 -0300

    4.7. Criando e mapeando a entidade de veículo

---
+++ src/main/java/com/home/transito/domain/model/StatusVeiculo.java
@@ -0,0 +1,5 @@
+package com.home.transito.domain.model;
+
+public enum StatusVeiculo {
+    REGULAR, APREENDIDO
+}
---
+++ src/main/java/com/home/transito/domain/model/Veiculo.java
@@ -0,0 +1,33 @@
+package com.home.transito.domain.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Veiculo {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+//    @JoinColumn(name = "proprietario_id")
+    private Proprietario proprietario;
+
+    private String marca;
+    private String placa;
+    private String modelo;
+
+    @Enumerated(EnumType.STRING)
+    private StatusVeiculo status;
+
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataApreensao;
+}
