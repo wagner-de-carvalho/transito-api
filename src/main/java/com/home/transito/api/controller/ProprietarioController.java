eaaa3c3659bc4bd1c8f0f66fbb937c1fa0e1ed62
wagner <wagncarv@outlook.com>
2023-05-28 00:24:54 -0300

    3.10. Implementando endpoint de busca de recurso

--- src/main/java/com/home/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/home/transito/api/controller/ProprietarioController.java
@@ -4,7 +4,9 @@ import com.home.transito.domain.model.Proprietario;
 import com.home.transito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +19,15 @@ public class ProprietarioController {
 
     private final ProprietarioRepository proprietarioRepository;
 
-    @GetMapping()
+    @GetMapping
     public List<Proprietario> testar() {
         return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                       .map(ResponseEntity::ok)
+                       .orElse(ResponseEntity.notFound().build());
     }
 }
