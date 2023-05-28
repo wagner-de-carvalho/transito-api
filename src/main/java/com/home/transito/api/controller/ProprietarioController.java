5ae7aa4d6c569892c9816c86db1c8eebc1abe72a
wagner <wagncarv@outlook.com>
2023-05-28 00:35:25 -0300

    3.11. Implementando endpoint de inclusão de recurso

--- src/main/java/com/home/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/home/transito/api/controller/ProprietarioController.java
@@ -4,11 +4,9 @@ import com.home.transito.domain.model.Proprietario;
 import com.home.transito.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,11 @@ public class ProprietarioController {
         return proprietarioRepository.findById(proprietarioId)
                        .map(ResponseEntity::ok)
                        .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 }
