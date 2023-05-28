2b889fcf8ff7a1fab4ef5dc0b817d138a111f7ec
wagner <wagncarv@outlook.com>
2023-05-28 00:12:08 -0300

    3.9. Implementando Query Methods no repositório

--- src/main/java/com/home/transito/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/home/transito/domain/repository/ProprietarioRepository.java
@@ -4,6 +4,10 @@ import com.home.transito.domain.model.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
+    List<Proprietario> findByNome(String nome);
+    List<Proprietario> findByNomeContaining(String nome);
 }
