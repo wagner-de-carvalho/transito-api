8c24a45bbb72b3400cebc9ebfa42efe8308047e2
wagner <wagncarv@outlook.com>
2023-05-28 01:02:47 -0300

    3.13. Implementando endpoint de exclusão de recurso

--- src/main/java/com/home/transito/api/controller/ProprietarioController.java
+++ src/main/java/com/home/transito/api/controller/ProprietarioController.java
@@ -45,4 +45,13 @@ public class ProprietarioController {
 
         return ResponseEntity.ok(proprietarioAtualizado);
     }
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> remover(@PathVariable Long proprietarioId) {
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+        proprietarioRepository.deleteById(proprietarioId);
+        return ResponseEntity.noContent().build();
+    }
 }
