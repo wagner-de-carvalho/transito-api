626dec95fb8969ae70e21862d40792340f5570f3
wagner <wagncarv@outlook.com>
2023-05-29 14:23:27 -0300

    6.3. Modelando sub-recursos

---
+++ src/main/java/com/home/transito/api/assembler/AutuacaoAssembler.java
@@ -0,0 +1,35 @@
+package com.home.transito.api.assembler;
+
+import com.home.transito.api.model.AutuacaoModel;
+import com.home.transito.api.model.VeiculoInput;
+import com.home.transito.api.model.VeiculoModel;
+import com.home.transito.api.model.input.AutuacaoInput;
+import com.home.transito.domain.model.Autuacao;
+import com.home.transito.domain.model.Veiculo;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Component
+public class AutuacaoAssembler {
+
+    private final ModelMapper modelMapper;
+
+    public Autuacao toEntity(AutuacaoInput autuacaoInput) {
+        return modelMapper.map(autuacaoInput, Autuacao.class);
+    }
+
+    public AutuacaoModel toModel(Autuacao autuacao) {
+        return modelMapper.map(autuacao, AutuacaoModel.class);
+    }
+
+    public List<AutuacaoModel> toCollectionModel(List<Autuacao> autuacoes) {
+        return autuacoes
+                       .stream()
+                       .map(this::toModel)
+                       .toList();
+    }
+}
---
+++ src/main/java/com/home/transito/api/model/input/AutuacaoInput.java
@@ -0,0 +1,20 @@
+package com.home.transito.api.model.input;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class AutuacaoInput {
+    @NotBlank
+    private String descricao;
+
+    @NotNull
+    @Positive
+    private BigDecimal valorMulta;
+}
