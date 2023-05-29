8b943cf5e6f23fe304a6aa28b076bb057a4014ef
wagner <wagncarv@outlook.com>
2023-05-28 23:34:27 -0300

    5.4. Adicionando campos customizados no ProblemDetail

--- src/main/java/com/home/transito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/home/transito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.home.transito.api.exceptionhandler;
 
 import com.home.transito.domain.exception.NegocioException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,6 +14,8 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
@@ -21,6 +25,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatus(status);
         problemDetail.setTitle("Um ou mais campos estão inválidos");
         problemDetail.setType(URI.create("http://hometransito.com/erros/campos-invalidos"));
+
+        Map<String, String> fields = ex.getBindingResult().getAllErrors()
+                                             .stream()
+                                             .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(),
+                                                     DefaultMessageSourceResolvable::getDefaultMessage));
+        problemDetail.setProperty("fields", fields);
 
         return handleExceptionInternal(ex, problemDetail, headers, status, request);
     }
