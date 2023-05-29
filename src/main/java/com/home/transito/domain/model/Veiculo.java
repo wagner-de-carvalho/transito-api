49e2f29952ecae1f1beb2139ed41777e2a2361e5
wagner <wagncarv@outlook.com>
2023-05-29 16:06:27 -0300

    6.8. Modelando ações não-CRUD

--- src/main/java/com/home/transito/domain/model/Veiculo.java
+++ src/main/java/com/home/transito/domain/model/Veiculo.java
@@ -1,5 +1,6 @@
 package com.home.transito.domain.model;
 
+import com.home.transito.domain.exception.NegocioException;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -42,5 +43,30 @@ public class Veiculo {
         getAutuacoes().add(autuacao);
 
         return autuacao;
+    }
+
+    public void apreender() {
+        if (estaApreendido()) {
+            throw new NegocioException("Veículo já se encontra apreendido");
+        }
+
+        setStatus(StatusVeiculo.APREENDIDO);
+        setDataApreensao(OffsetDateTime.now());
+    }
+
+    public void removerApreensao() {
+        if (naoEstaApreendido()) {
+            throw new NegocioException("Veículo não está apreendido");
+        }
+        setStatus(StatusVeiculo.REGULAR);
+        setDataApreensao(null);
+    }
+
+    public boolean estaApreendido() {
+        return StatusVeiculo.APREENDIDO.equals(getStatus());
+    }
+
+    public boolean naoEstaApreendido() {
+        return !estaApreendido();
     }
 }
---
+++ src/main/java/com/home/transito/domain/service/ApreensaoVeiculoService.java
@@ -0,0 +1,25 @@
+package com.home.transito.domain.service;
+
+import com.home.transito.domain.model.Veiculo;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class ApreensaoVeiculoService {
+
+    private final RegistroVeiculoService registroVeiculoService;
+
+    @Transactional
+    public void apreender(Long veiculoId) {
+        Veiculo veiculo = registroVeiculoService.buscar(veiculoId);
+        veiculo.apreender();
+    }
+
+    @Transactional
+    public void removerApreensao(Long veiculoId) {
+        Veiculo veiculo = registroVeiculoService.buscar(veiculoId);
+        veiculo.removerApreensao();
+    }
+}
