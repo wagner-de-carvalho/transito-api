a65c988eca134ba39f58acb770f98ca75c8c131c
wagner <wagner.carvalho@outlook.com>
2024-02-15 19:06:17 -0300

    Implementing create Proprietario resource endpoint

--- src/main/java/com/carvalho/transito/api/controllers/ProprietarioController.java
+++ src/main/java/com/carvalho/transito/api/controllers/ProprietarioController.java
@@ -2,10 +2,14 @@ package com.carvalho.transito.api.controllers;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carvalho.transito.domain.model.Proprietario;
@@ -30,6 +34,12 @@ public class ProprietarioController {
         return proprietarioRepository.findById(proprietarioId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
+        return proprietarioRepository.save(proprietario);
     }
 
 }
