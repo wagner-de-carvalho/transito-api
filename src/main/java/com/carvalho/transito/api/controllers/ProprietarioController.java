3309cc6f6e5338a886b4342cd6783d4d8596976d
wagner <wagner.carvalho@outlook.com>
2024-02-15 18:55:11 -0300

    Implementig get  Proprietario by id resource

--- src/main/java/com/carvalho/transito/api/controllers/ProprietarioController.java
+++ src/main/java/com/carvalho/transito/api/controllers/ProprietarioController.java
@@ -2,7 +2,9 @@ package com.carvalho.transito.api.controllers;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,13 @@ public class ProprietarioController {
     @GetMapping()
     public List<Proprietario> listar() {
         return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
