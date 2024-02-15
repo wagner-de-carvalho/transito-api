21a08e9e6a0b835146c6e7dfa169c26ad7a5db48
wagner <wagner.carvalho@outlook.com>
2024-02-15 19:30:25 -0300

    Implementig delete Proprietario by id  endpoint

--- src/main/java/com/carvalho/transito/api/controllers/ProprietarioController.java
+++ src/main/java/com/carvalho/transito/api/controllers/ProprietarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -53,6 +54,15 @@ public class ProprietarioController {
         var proprietarioAtualizado = proprietarioRepository.save(proprietario);
 
         return ResponseEntity.ok(proprietarioAtualizado);
+    }
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> remover(@PathVariable Long proprietarioId) {
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+        proprietarioRepository.deleteById(proprietarioId);
+        return ResponseEntity.noContent().build();
     }
 
 }
