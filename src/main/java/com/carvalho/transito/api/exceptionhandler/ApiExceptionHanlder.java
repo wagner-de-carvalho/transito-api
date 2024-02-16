768056e20ccf0fb3836260bf2142506f027948f5
wagner <wagner.carvalho@outlook.com>
2024-02-16 20:11:10 -0300

    Adding costumized fields for ProblemDetail

--- src/main/java/com/carvalho/transito/api/exceptionhandler/ApiExceptionHanlder.java
+++ src/main/java/com/carvalho/transito/api/exceptionhandler/ApiExceptionHanlder.java
@@ -1,11 +1,14 @@
 package com.carvalho.transito.api.exceptionhandler;
 
 import java.net.URI;
+import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -23,6 +26,14 @@ public class ApiExceptionHanlder extends ResponseEntityExceptionHandler {
         ProblemDetail problemDetail = ProblemDetail.forStatus(status);
         problemDetail.setTitle("Um ou mais campos estão inválidos");
         problemDetail.setType(URI.create("https://transito-api.com.br/erros/campos-invalidos"));
+
+        var fields = ex.getBindingResult().getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(
+                        objectError -> ((FieldError) objectError).getField(),
+                        DefaultMessageSourceResolvable::getDefaultMessage));
+
+        problemDetail.setProperty("fields", fields);
 
         return handleExceptionInternal(ex, problemDetail, headers, status, request);
     }
