cfa755afbf173260ed0b771e03458faf35190a7f
wagner <wagner.carvalho@outlook.com>
2024-02-16 18:24:17 -0300

    Catching global exceptions with @RestControllerAdvice

---
+++ src/main/java/com/carvalho/transito/api/exceptionhandler/ApiExceptionHanlder.java
@@ -0,0 +1,17 @@
+package com.carvalho.transito.api.exceptionhandler;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.carvalho.transito.domain.exception.NegocioException;
+
+@RestControllerAdvice
+public class ApiExceptionHanlder {
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<String> capturar(NegocioException e) {
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+}
