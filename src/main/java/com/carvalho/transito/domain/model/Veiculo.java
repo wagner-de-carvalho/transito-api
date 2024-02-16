af71f2daa9fb8219e0cfcb2614b693d89736e145
wagner <wagner.carvalho@outlook.com>
2024-02-16 10:28:59 -0300

    Adding validations on Veiculo

--- src/main/java/com/carvalho/transito/domain/model/Veiculo.java
+++ src/main/java/com/carvalho/transito/domain/model/Veiculo.java
@@ -12,6 +12,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -26,10 +29,18 @@ public class Veiculo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
     @ManyToOne
     private Proprietario proprietario;
+
+    @NotBlank
     private String marca;
+
+    @NotBlank
     private String modelo;
+
+    @NotBlank
+    @Pattern(regexp = "[A-Za-z]{3}[0-9][0-9A-Za-z][0-9]{2}")
     private String placa;
 
     @JsonProperty(access = Access.READ_ONLY)
