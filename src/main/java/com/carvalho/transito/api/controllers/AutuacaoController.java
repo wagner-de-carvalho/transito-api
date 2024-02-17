fdd116828294fd74a7a16c2d13fd33b47da8826c
wagner <wagner.carvalho@outlook.com>
2024-02-17 14:38:28 -0300

    Implementing autuacao collection feature

--- src/main/java/com/carvalho/transito/api/controllers/AutuacaoController.java
+++ src/main/java/com/carvalho/transito/api/controllers/AutuacaoController.java
@@ -1,6 +1,9 @@
 package com.carvalho.transito.api.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +15,7 @@ import com.carvalho.transito.api.assembler.AutuacaoAssembler;
 import com.carvalho.transito.api.model.AutuacaoModel;
 import com.carvalho.transito.api.model.input.AutuacaoInput;
 import com.carvalho.transito.domain.service.RegistroAutuacaoService;
+import com.carvalho.transito.domain.service.RegistroVeiculoService;
 
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
@@ -22,6 +26,7 @@ import lombok.AllArgsConstructor;
 public class AutuacaoController {
     private final AutuacaoAssembler autuacaoAssembler;
     private final RegistroAutuacaoService registroAutuacaoService;
+    private final RegistroVeiculoService registroVeiculoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -30,6 +35,12 @@ public class AutuacaoController {
         var autuacaoRegistrada = registroAutuacaoService.registrar(veiculoId, autuacao);
 
         return autuacaoAssembler.toModel(autuacaoRegistrada);
+    }
+
+    @GetMapping
+    public List<AutuacaoModel> listar(@PathVariable Long veiculoId) {
+        var veiculo = registroVeiculoService.buscar(veiculoId);
+        return autuacaoAssembler.toCollectionModel(veiculo.getAutuacoes());
     }
 
 }
