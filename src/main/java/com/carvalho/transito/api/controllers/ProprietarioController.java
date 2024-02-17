0ef2f7aadee895d2b46defb21548451047446074
wagner <wagner.carvalho@outlook.com>
2024-02-16 23:47:49 -0300

    REmoving unused imports

--- src/main/java/com/carvalho/transito/api/controllers/ProprietarioController.java
+++ src/main/java/com/carvalho/transito/api/controllers/ProprietarioController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.carvalho.transito.domain.exception.NegocioException;
 import com.carvalho.transito.domain.model.Proprietario;
 import com.carvalho.transito.domain.repository.ProprietarioRepository;
 import com.carvalho.transito.domain.service.RegistroProprietarioService;
--- src/main/java/com/carvalho/transito/api/controllers/VeiculoController.java
+++ src/main/java/com/carvalho/transito/api/controllers/VeiculoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.carvalho.transito.api.model.VeiculoModel;
-import com.carvalho.transito.domain.exception.NegocioException;
 import com.carvalho.transito.domain.model.Veiculo;
 import com.carvalho.transito.domain.repository.VeiculoRepository;
 import com.carvalho.transito.domain.service.RegistroVeiculoService;
