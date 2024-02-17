5ec520263b92687b4a75417d48cb8e448bdcc58f
wagner <wagner.carvalho@outlook.com>
2024-02-17 12:52:55 -0300

    Creating and mapping the autuacao entity

---
+++ src/main/java/com/carvalho/transito/domain/model/Autuacao.java
@@ -0,0 +1,30 @@
+package com.carvalho.transito.domain.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Autuacao {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String descricao;
+    private BigDecimal valorMulta;
+    private OffsetDateTime dataOcorrencia;
+
+    @ManyToOne
+    private Veiculo veiculo;
+}
--- src/main/java/com/carvalho/transito/domain/model/Veiculo.java
+++ src/main/java/com/carvalho/transito/domain/model/Veiculo.java
@@ -1,6 +1,8 @@
 package com.carvalho.transito.domain.model;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -9,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,6 +35,9 @@ public class Veiculo {
 
     @Enumerated(EnumType.STRING)
     private StatusVeiculo status;
+
+    @OneToMany(mappedBy = "veiculo")
+    private List<Autuacao> autuacoes = new ArrayList<>();
 
     private OffsetDateTime dataCadastro;
     private OffsetDateTime dataApreensao;
